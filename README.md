d22ef0dfb5476d7f3bcac37cb1293451de015535
Pavel <volkov080693@gmail.com>
2021-04-09 16:00:07 +0300

    server and response apps

---
+++ README.md
@@ -0,0 +1,2 @@
+Server - app for send code 200 to 8000 port, include in containder without livenes prode
+Response - app for check code 200 from url when Server app working
---
+++ response.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://$paste_your_URL_here")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp.StatusCode)
+}
